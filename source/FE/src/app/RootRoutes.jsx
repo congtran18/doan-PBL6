4828c52fefc625ab58296d4da37fd949353f1d77
CONGTRAN <trung@spritely.co>
2021-12-26 17:59:12 +0700

    first commit

---
+++ source/FE/src/app/RootRoutes.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Redirect } from 'react-router-dom'
+
+import dashboardRoutes from './views/dashboard/DashboardRoutes'
+import utilitiesRoutes from './views/utilities/UtilitiesRoutes'
+
+import materialRoutes from './views/material-kit/MaterialRoutes'
+import chartsRoute from './views/charts/ChartsRoute'
+import dragAndDropRoute from './views/Drag&Drop/DragAndDropRoute'
+
+import ProductsRoutes from './views/products/ProductsRoutes'
+import OrdersRoutes from './views/orders/OrdersRoutes'
+import AccountsRoutes from './views/accounts/AccountsRoutes'
+
+import formsRoutes from './views/forms/FormsRoutes'
+import mapRoutes from './views/map/MapRoutes'
+
+
+const redirectRoute = [
+    {
+        path: '/',
+        exact: true,
+        component: () => <Redirect to="/dashboard/default" />,
+    },
+]
+
+const errorRoute = [
+    {
+        component: () => <Redirect to="/session/404" />,
+    },
+]
+
+const routes = [
+    ...dashboardRoutes,
+    ...materialRoutes,
+    ...utilitiesRoutes,
+    ...chartsRoute,
+    ...dragAndDropRoute,
+    ...ProductsRoutes,
+    ...OrdersRoutes,
+    ...AccountsRoutes,
+    ...formsRoutes,
+    ...mapRoutes,
+    ...redirectRoute,
+    ...errorRoute,
+]
+
+export default routes
